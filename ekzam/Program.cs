a89488dbd9f70dce6834c76de051eb9131890748
Gera <sergeev9927@gmail.com>
2023-12-14 18:03:37 +0300

    Первая задача

---
+++ ekzam/Program.cs
@@ -0,0 +1,19 @@
+﻿// ЗАДАЧА 1
+
+// Задайте значения A и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от A до N. 
+// Использовать рекурсию, не использовать циклы.
+
+System.Console.Write("Введите значение М: ");
+int A = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите значение N: ");
+int B = Convert.ToInt32(Console.ReadLine());
+void Numbers (int A)
+{
+    if (A <= B)
+    {
+        Console.Write("{0} ", A);
+        Numbers(A + 1);
+    }
+}
+Numbers(A);
